3b403a76314b14d9c679e859233500750fb3357f
suhanikhetpalia <70369733+suhanikhetpalia@users.noreply.github.com>
2022-04-17 23:06:15 +0530

    blog list functions

---
+++ js/bloglist.js
@@ -0,0 +1,22 @@
+var trashFunctions = function(numberOfPosts) {
+
+    for (var i = 0; i < numberOfPosts; i++) {
+        var modal = document.getElementsByClassName('trash-modal')[i];
+        var div = document.getElementsByClassName("post-trash")[i];
+        var close = document.getElementsByClassName("trash-modal-button-no")[i];
+        div.onclick = function() {
+            modal.style.display = "block";
+        }
+
+
+        close.onclick = function() {
+            modal.style.display = "none";
+        }
+    }
+}
+
+trashFunctions(5)
+
+function navigateToPost() {
+    location.href = "postslist.html";
+}
